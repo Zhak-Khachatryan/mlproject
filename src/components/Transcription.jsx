226095bdbe29194d2f827a6e109f62a26521d971
JakAsAlways <mrjak366@gmail.com>
2024-08-20 15:16:48 +0400

    testing to find the issue

---
+++ src/components/Transcription.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Transcription(props) {
+  const {output} = props
+
+  console.log(output)
+  const finalText = output.map(val => val.text)
+
+  return (
+    <div>
+      {finalText}
+    </div>
+  )
+}
